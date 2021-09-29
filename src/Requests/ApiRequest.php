b33e5bd2135dc75b224ffc82e8b50716ce8095ce
Nicoorfi <nico@sigmie.com>
2021-09-29 12:51:15 +0200

    FIX: request body

--- src/Requests/ApiRequest.php
+++ src/Requests/ApiRequest.php
@@ -13,6 +13,6 @@ class ApiRequest extends JSONRequest
     {
         $uri = new Uri("/api/{$uri}");
 
-        parent::__construct($method, $uri, $this->headers, ['bar']);
+        parent::__construct($method, $uri, $body);
     }
 }
